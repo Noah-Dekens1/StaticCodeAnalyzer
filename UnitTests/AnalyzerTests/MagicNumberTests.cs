23f8243b79b4daca2985e983cbe71011b1ae1644
Noah Dekens <noah.dekens@infosupport.com>
2024-04-24 13:39:04 +0200

    Add tests for magic number & nested ternary analyzers

---
+++ UnitTests/AnalyzerTests/MagicNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InfoSupport.StaticCodeAnalyzer.Application.StaticCodeAnalysis.Analysis.Analyzers;
+using InfoSupport.StaticCodeAnalyzer.UnitTests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyzerTests;
+
+[TestClass]
+public class MagicNumberTests
+{
+    [TestMethod]
+    public void Analyze_MagicNumberInMethodCall_ReturnsIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            someMethod(30);
+            """, new MagicNumberAnalyzer());
+
+        Assert.AreEqual(1, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_NamedNumberInMethodCall_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            someMethod(count: 30);
+            """, new MagicNumberAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+}
---
+++ UnitTests/AnalyzerTests/NestedTernaryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InfoSupport.StaticCodeAnalyzer.Application.StaticCodeAnalysis.Analysis.Analyzers;
+using InfoSupport.StaticCodeAnalyzer.UnitTests.Utils;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnalyzerTests;
+
+[TestClass]
+public class NestedTernaryTests
+{
+    [TestMethod]
+    public void Analyze_NestedTernary_ReturnsIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            var a = a ? b : c ? d : e;
+            """, new NestedTernaryAnalyzer());
+
+        Assert.AreEqual(1, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_RegularTernary_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            var a = a ? b : c;
+            """, new NestedTernaryAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+}
