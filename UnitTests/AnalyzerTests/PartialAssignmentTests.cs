f37897fa7d0f6bcc56a332112623430f5801b57a
Noah Dekens <noah.dekens@infosupport.com>
2024-04-25 09:46:17 +0200

    Add partial variable assignment tests

---
+++ UnitTests/AnalyzerTests/PartialAssignmentTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InfoSupport.StaticCodeAnalyzer.Application.StaticCodeAnalysis.Analysis.Analyzers;
+using InfoSupport.StaticCodeAnalyzer.UnitTests.Utils;
+
+namespace AnalyzerTests;
+
+[TestClass]
+public class PartialAssignmentTests
+{
+    [TestMethod]
+    public void Analyze_RegularVariableDeclaration_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            var a = 0;
+            """, new PartialVariableAssignmentAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_RegularMultiVariableDeclaration_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            int a = 0, b = 0;
+            """, new PartialVariableAssignmentAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_VariableDeclarationPartialAssignment_ReturnsIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            int a, b = 0;
+            """, new PartialVariableAssignmentAnalyzer());
+
+        Assert.AreEqual(1, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_SingleDeclarator_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            int a;
+            """, new PartialVariableAssignmentAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+
+    [TestMethod]
+    public void Analyze_MultipleDeclaratorOnly_ReturnsNoIssue()
+    {
+        var issues = AnalyzerUtils.Analyze("""
+            int a, b;
+            """, new PartialVariableAssignmentAnalyzer());
+
+        Assert.AreEqual(0, issues.Count);
+    }
+}
