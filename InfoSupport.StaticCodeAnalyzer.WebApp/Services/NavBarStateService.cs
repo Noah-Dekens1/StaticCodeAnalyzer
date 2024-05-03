dd8c9269be3eefee15c73f334168a34b9479476f
Noah Dekens <noah.dekens@infosupport.com>
2024-05-03 14:56:19 +0200

    Add page titles/redirects

--- InfoSupport.StaticCodeAnalyzer.WebApp/Services/NavBarStateService.cs
+++ InfoSupport.StaticCodeAnalyzer.WebApp/Services/NavBarStateService.cs
@@ -1,7 +1,21 @@
 ﻿namespace InfoSupport.StaticCodeAnalyzer.WebApp.Services;
 
+public class PageTitleEventArgs(string title, string redirect) : EventArgs
+{
+    public string Title { get; set; } = title;
+    public string Redirect { get; set; } = redirect;
+}
+
 public class NavBarStateService
 {
     public string PageTitle { get; set; } = "";
     public string TitleRedirect { get; set; } = "/";
+
+    public event EventHandler<PageTitleEventArgs>? OnPageTitleUpdate;
+
+    public void UpdateTitle(string title, string redirect="/")
+    {
+        PageTitle = title;
+        OnPageTitleUpdate?.Invoke(this, new PageTitleEventArgs(title, redirect));
+    }
 }
